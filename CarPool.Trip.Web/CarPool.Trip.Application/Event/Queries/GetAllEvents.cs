ad682aca52937ba82314b6b7b45cf0084fa0431e
Jonas Motiejauskas <jonasmotiejauskas@gmail.com>
2020-02-29 16:02:41 +0200

    Hardcoded event but added to DB

--- CarPool.Trip.Web/CarPool.Trip.Application/Event/Queries/GetAllEvents.cs
+++ CarPool.Trip.Web/CarPool.Trip.Application/Event/Queries/GetAllEvents.cs
@@ -23,17 +23,17 @@ namespace CarPool.Trip.Application.Event.Queries
                 _tripDb = tripDb;
             }
 
-            public Task<IEnumerable<EventDto>> Handle(GetAllEvents request, CancellationToken cancellationToken)
-            {
-                return Task.FromResult(new EventDto[] {new EventDto
-                {
-                    Id = 1,
-                    EventName = "Hack4Climate",
-                    Description = "#Hack4Climate dalyvauti kviečiame IT sektoriaus profesionalus, dizainerius, viešųjų ryšių specialistus, teisininkus, architektus, inžinierius, mokytojus, studentus, moksleivius ir visus kitus, kuriuos domina ekologiškos technologijos ir tvarūs sprendimai.",
-                    Address = "Gedimino pr. 16, 01103 Vilnius, Lithuania",
-                    LogoUri = "https://scontent.fvno5-1.fna.fbcdn.net/v/t1.0-9/82537611_10157898834144233_5570923447380344832_o.jpg?_nc_cat=104&_nc_sid=b386c4&_nc_ohc=CeBE5E9omLcAX-UBwpW&_nc_ht=scontent.fvno5-1.fna&oh=fac56b3e335d3747ab7e8bf6dd258f2e&oe=5EFF9DD3"
-                }}.AsEnumerable());
-            }
+            public async Task<IEnumerable<EventDto>> Handle(GetAllEvents request, CancellationToken cancellationToken)
+                 => _tripDb.Events.Select(x =>
+                 new EventDto
+                 {
+                     Id = x.Id,
+                     EventName = x.EventName,
+                     Description = x.Description,
+                     Address = x.Address,
+                     LogoUri = x.LogoUri
+                 });
+            
         }
     }
 }
--- CarPool.Trip.Web/CarPool.Trip.Web/Startup.cs
+++ CarPool.Trip.Web/CarPool.Trip.Web/Startup.cs
@@ -1,3 +1,4 @@
+﻿using CarPool.Trip.Domain.Entities;
 using CarPool.Trip.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -35,7 +36,21 @@ namespace CarPool.Trip.Web
         {
             using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             using (var context = scope.ServiceProvider.GetService<TripDbContext>())
+            {
                 context.Database.Migrate();
+                if (context.Events.CountAsync().GetAwaiter().GetResult() == 0) 
+                {
+                    context.Events.Add(new Event
+                    {
+                        Id = 1,
+                        EventName = "Hack4Climate",
+                        Description = "#Hack4Climate dalyvauti kviečiame IT sektoriaus profesionalus, dizainerius, viešųjų ryšių specialistus, teisininkus, architektus, inžinierius, mokytojus, studentus, moksleivius ir visus kitus, kuriuos domina ekologiškos technologijos ir tvarūs sprendimai.",
+                        Address = "Gedimino pr. 16, 01103 Vilnius, Lithuania",
+                        LogoUri = "https://scontent.fvno5-1.fna.fbcdn.net/v/t1.0-9/82537611_10157898834144233_5570923447380344832_o.jpg?_nc_cat=104&_nc_sid=b386c4&_nc_ohc=CeBE5E9omLcAX-UBwpW&_nc_ht=scontent.fvno5-1.fna&oh=fac56b3e335d3747ab7e8bf6dd258f2e&oe=5EFF9DD3"
+                    });
+                    context.SaveChanges(true);
+                }
+            }
 
             if (env.IsDevelopment())
             {
