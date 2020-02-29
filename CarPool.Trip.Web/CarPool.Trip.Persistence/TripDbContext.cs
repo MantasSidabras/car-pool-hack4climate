2385fc07dcd5cf961aafc17042ae72f3a71ec3d2
Povilas Zvirblis <povilas.zvirblis1@gmail.com>
2020-02-29 14:06:07 +0200

    TripDbContext added

--- CarPool.Trip.Web/CarPool.Trip.Persistence/TripDbContext.cs
+++ CarPool.Trip.Web/CarPool.Trip.Persistence/TripDbContext.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using CarPool.Trip.Domain.Entities;
+using CarPool.Trip.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarPool.Trip.Persistence
 {
     public class TripDbContext : DbContext
     {
-        protected TripDbContext()
+        protected TripDbContext(DbContextOptions<TripDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventTrip> EventTrips { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<TripJoinRequest> TripJoinRequests { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new EventConfiguration());
+            modelBuilder.ApplyConfiguration(new EventTripConfiguration());
+            modelBuilder.ApplyConfiguration(new ParticipantConfiguration());
+            modelBuilder.ApplyConfiguration(new TripJoinRequestConfiguration());
         }
     }
 }
