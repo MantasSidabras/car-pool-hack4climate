d08f7437f22577649a5f9f322761b62567d55ad4
Jonas Motiejauskas <jonasmotiejauskas@gmail.com>
2020-02-29 21:29:42 +0200

    Added Login

---
+++ CarPool.Trip.Web/CarPool.Trip.Application/Dto/ParticipantDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CarPool.Trip.Application.Dto
+{
+    public class ParticipantDto
+    {
+        public string AuthToken { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Carplate { get; set; }
+        public string CarModel { get; set; }
+    }
+}
