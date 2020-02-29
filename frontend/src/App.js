323f306418dd05d20ee64b58dd616658ea9a3732
Matas Ramanauskas <matasramanauskas@gmail.com>
2020-02-29 17:37:36 +0200

    axios added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-
-import AppBarComponent from './Components/AppBar/AppBar';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core';
-import { blue, yellow, lightBlue } from '@material-ui/core/colors';
-import MainSection from './Components/MainSection/MainSection';
+import AppBarComponent from "./Components/AppBar/AppBar";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
+import { blue, yellow, lightBlue } from "@material-ui/core/colors";
+import MainSection from "./Components/MainSection/MainSection";
+import { getEvents } from "./services/axios";
 
 const theme = createMuiTheme({
   palette: {
     primary: lightBlue
-  },
+  }
 });
 
 const App = () => {
+  // const [data, setData] = useState([]);
+  // const [error, setError] = useState();
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const { data, error } = await getEvents();
+  //     if (data) {
+  //       setData(data);
+  //     }
+  //     if (error) {
+  //       setError(error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
@@ -23,6 +40,6 @@ const App = () => {
       </div>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
