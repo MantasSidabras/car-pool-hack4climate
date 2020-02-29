cff4e45dcb38c79f864870a4374c8afb97b33910
Jonas Motiejauskas <jonasmotiejauskas@gmail.com>
2020-02-29 12:31:59 +0200

    removed swagger

--- CarPool.Trip.Web/CarPool.Trip.Web/Program.cs
+++ CarPool.Trip.Web/CarPool.Trip.Web/Program.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Serilog;
 
 namespace CarPool.Trip.Web
 {
@@ -19,10 +12,6 @@ namespace CarPool.Trip.Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog((hostingContext, loggerConfiguration) =>
-                {
-                    loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
