dda99d99dabbf33f0ecbdfc58fc84b428e204af3
Povilas Zvirblis <povilas.zvirblis1@gmail.com>
2020-03-01 01:11:12 +0200

    cheat endpoint

---
+++ CarPool.Trip.Web/CarPool.Trip.Application/Dto/ParticipantDetailedDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarPool.Trip.Application.Dto
+{
+    public class ParticipantDetailedDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Carplate { get; set; }
+        public string CarModel { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ CarPool.Trip.Web/CarPool.Trip.Application/Participant/Queries/GetParticipants.cs
@@ -0,0 +1,38 @@
+﻿using CarPool.Trip.Application.Dto;
+using CarPool.Trip.Persistence;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CarPool.Trip.Application.Participant.Queries
+{
+    public class GetParticipants : IRequest<IEnumerable<ParticipantDetailedDto>>
+    {
+        public class Handler : IRequestHandler<GetParticipants, IEnumerable<ParticipantDetailedDto>>
+        {
+            private readonly TripDbContext _dbContext;
+
+            public Handler(TripDbContext dbContext)
+            {
+                _dbContext = dbContext;
+            }
+
+            public async Task<IEnumerable<ParticipantDetailedDto>> Handle(GetParticipants request, CancellationToken cancellationToken)
+            {
+                return _dbContext.Participants
+                    .Select(x => new ParticipantDetailedDto
+                    {
+                        CarModel = x.CarModel,
+                        Carplate = x.Carplate,
+                        Id = x.Id,
+                        Name = x.Name,
+                        Password = x.Password,
+                        PhoneNumber = x.PhoneNumber,
+                        Surname = x.Surname
+                    });
+            }
+        }
+    }
+}
