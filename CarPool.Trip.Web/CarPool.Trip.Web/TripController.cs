07630b3680fcb3b075c73e9917723f0f906c6ac5
Jonas Motiejauskas <jonasmotiejauskas@gmail.com>
2020-02-29 13:31:40 +0200

    change on entities and controllers added

--- CarPool.Trip.Web/CarPool.Trip.Web/TripController.cs
+++ CarPool.Trip.Web/CarPool.Trip.Web/TripController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarPool.Trip.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace CarPool.Trip.Web
 {
@@ -9,6 +11,24 @@ namespace CarPool.Trip.Web
         public IActionResult Health()
         {
             return Ok("Healthy");
+        }
+
+        [HttpGet("events")]
+        public Event[] Events()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("trips")]
+        public EventTrip[] Trips()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("trip/init")]
+        public IActionResult InitiateTrip()
+        {
+            throw new NotImplementedException();
         }
     }
 }
